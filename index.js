af07929970a798debfd2ae876ca675ae0af43ac3
almahfuz <almahfuz0327@gmail.com>
2022-07-30 00:38:31 +0600

    added start button, animations

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 var trexImg = document.getElementsByClassName("t-rex")[0];
 var jumpBtn = document.getElementById("jump-btn");
+var startBtn = document.getElementById("start-btn");
+var spike = document.getElementById("spikeIcon");
+
+var count = 1;
+var inc = 0;
+
+startBtn.addEventListener("click", function () {
+    spike.classList.add("start-anim");
+    positionOfTrexSpike();
+
+});
+
+function positionOfTrexSpike() {
+
+
+    var trexX;
+    var trexY;
+    var spikeX;
+    var spikeY;
+
+
+    setTimeout(() => {
+
+        let rect = trexImg.getBoundingClientRect();
+        let rect2 = spike.getBoundingClientRect();
+
+        trexX = rect.left;
+        trexY = rect.top;
+        spikeX = rect2.left;
+        spikeY = rect2.top;
+        if (spikeY == trexY) {
+            spike.style.animationPlayState = "paused";
+            trexImg.classList.add("t-rex-failed");
+            setTimeout(() => {
+                spike.style.animationPlayState = "running";
+                spike.classList.remove("start-anim");
+
+                trexImg.classList.remove("t-rex-failed");
+            }, 2000);
+        }
+        else {
+            count++;
+            inc = 200;
+            positionOfTrexSpike();
+
+        }
+
+    }, (3800) + inc);
+};
 
 jumpBtn.addEventListener("click", function () {
 
